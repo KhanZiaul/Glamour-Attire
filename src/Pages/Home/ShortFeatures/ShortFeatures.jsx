093aeef0e6cae97ea85121bc46e041c841d4eefc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-25 14:08:42 +0600

    added data for ShortFeatures section

---
+++ src/Pages/Home/ShortFeatures/ShortFeatures.jsx
@@ -0,0 +1,24 @@
+import image1 from '../../../assets/img/features/f1.png'
+import image2 from '../../../assets/img/features/f2.png'
+import image3 from '../../../assets/img/features/f3.png'
+import image4 from '../../../assets/img/features/f4.png'
+import image5 from '../../../assets/img/features/f5.png'
+import image6 from '../../../assets/img/features/f6.png'
+
+const ShortFeatures = () => {
+    const features = [
+        {image:image1,name:'Free Shipping'},
+        {image:image2,name:'Online Order'},
+        {image:image3,name:'Save Money'},
+        {image:image4,name:'Promotions'},
+        {image:image5,name:'Happy Sell'},
+        {image:image6,name:'F24/7 Support'}
+    ]
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ShortFeatures;
