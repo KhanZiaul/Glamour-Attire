c518f4892cafc327d63ad9fab3943e881a6fed1f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-25 15:15:35 +0600

    done ShortFeatures section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import ShortFeatures from "../ShortFeatures/ShortFeatures";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <ShortFeatures></ShortFeatures>
         </div>
     );
 };
--- src/Pages/Home/ShortFeatures/ShortFeatures.jsx
+++ src/Pages/Home/ShortFeatures/ShortFeatures.jsx
@@ -7,16 +7,25 @@ import image6 from '../../../assets/img/features/f6.png'
 
 const ShortFeatures = () => {
     const features = [
-        {image:image1,name:'Free Shipping'},
-        {image:image2,name:'Online Order'},
-        {image:image3,name:'Save Money'},
-        {image:image4,name:'Promotions'},
-        {image:image5,name:'Happy Sell'},
-        {image:image6,name:'F24/7 Support'}
+        { image: image1, name: 'Free Shipping' },
+        { image: image2, name: 'Online Order' },
+        { image: image3, name: 'Save Money' },
+        { image: image4, name: 'Promotions' },
+        { image: image5, name: 'Happy Sell' },
+        { image: image6, name: 'F24/7 Support' }
     ]
     return (
-        <div>
-            
+        <div className='flex mx-auto w-[90%] justify-between my-8'>
+            {
+                features.map((feature, index) => {
+                    return (
+                        <div key={index} className='px-3 py-6 border-2 rounded-md shadow-xl'>
+                            <img src={feature.image} alt="" />
+                            <p className='text-center font-semibold mt-3 bg-[#164B60] shadow-xl text-white'>{feature.name}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
