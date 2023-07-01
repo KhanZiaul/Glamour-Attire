36695735f9f1aa77f2c3119f5a07a35edf610bb5
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-01 23:46:05 +0600

    firebase updated

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4dlIaAAtmRAcz4LbMn7dQdMkGHDjM_hw",
-  authDomain: "glamour-attire.firebaseapp.com",
-  projectId: "glamour-attire",
-  storageBucket: "glamour-attire.appspot.com",
-  messagingSenderId: "195436533034",
-  appId: "1:195436533034:web:2f46c32fa769b7fe443a53"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
