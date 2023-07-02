0a69d8966b6a9c69940eb48d934dde55b429e85f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-02 12:35:26 +0600

    AuthProvider  updated

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,30 +1,51 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/firebase.config";
 
 const AuthContext = createContext(null)
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const auth = getAuth(app);
-    const [user,setUser] = useState(null)
-    const [loading , setLoading] = useState(false)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    function createUser(email, password){
+    function createUser(email, password) {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    function loginUser(email, password){
+    function signinUser(email, password) {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
+    function signInPopup(provider) {
+        setLoading(true)
+        return signInWithPopup(auth, provider)
+    }
+
+    function logOut() {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+      const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
+            setLoading(false)
+            setUser(currentUser)
+        });
+        return ()=>{
+            return unsubscribe();
+        }
+    }, [auth])
+
     const authInfo = {
         user,
         createUser,
-        loginUser,
-        
+        signinUser,
+        signInPopup,
         loading,
+        logOut
     }
 
     return (
