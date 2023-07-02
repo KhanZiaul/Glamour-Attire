3ceeae8e115ad27929d941844fed3afee53616f5
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-03 00:14:02 +0600

    fetch data using query done

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+    const { id } = useParams()
+    console.log(id)
+
+    const { data } = useQuery({
+        queryKey: ['product'],
+        queryFn: async () => {
+            const data = await fetch(`http://localhost:3000/product/${id}`)
+            return data.json()
+        }
+    })
+
+    console.log(data)
+    
     return (
-        <div>
+        <div className="pt-16">
             <h2>Product Details</h2>
+            <img src={data?.img} alt="" />
         </div>
     );
 };
