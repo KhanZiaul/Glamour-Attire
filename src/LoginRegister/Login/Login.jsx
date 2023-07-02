8af3f7062f305007d6a74cfbfb298d56cb5d2f70
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-02 16:09:49 +0600

    Login updated

--- src/LoginRegister/Login/Login.jsx
+++ src/LoginRegister/Login/Login.jsx
@@ -2,8 +2,34 @@
 
 const Login = () => {
     return (
-        <div className="pt-20 mb-10">
-            <h2>login</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-3xl font-bold">LOGIN</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
