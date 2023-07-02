aa4663e5ab4a10671bc7958093ff06dfbf993ef7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-03 00:29:47 +0600

    ProductDetails updated

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -15,11 +15,34 @@ const ProductDetails = () => {
     })
 
     console.log(data)
-    
+
     return (
-        <div className="pt-16">
-            <h2>Product Details</h2>
-            <img src={data?.img} alt="" />
+        <div className="pt-24 flex items-center gap-10 mb-10 w-[95%] mx-auto">
+            <div>
+                <img className="rounded-xl" src={data?.img} alt="" />
+            </div>
+            <div className="space-y-4">
+                <p>Home/{data?.brand}</p>
+                <p>{data?.productName}</p>
+                <p>${data?.price}</p>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Normal</option>
+                    <option>Normal Apple</option>
+                    <option>Normal Orange</option>
+                    <option>Normal Tomato</option>
+                </select>
+                <div className="flex">
+                    <select className="select select-bordered w-full max-w-xs">
+                        <option disabled selected>Normal</option>
+                        <option>Normal Apple</option>
+                        <option>Normal Orange</option>
+                        <option>Normal Tomato</option>
+                    </select>
+                    <button className="btn btn-primary">Add To Cart</button>
+                </div>
+                <p>Product Details</p>
+                <p>{data?.productDetails}</p>
+            </div>
         </div>
     );
 };
