abd91719fc084a201ccfd034d1bc3508cc165042
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-03 09:56:19 +0600

    useAdmin custom hook added

---
+++ src/CustomHook/useAdmin/useAdmin.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../useAxiosSecure/useAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
+
+function useAdmin(){
+    const [axiosSecure] = useAxiosSecure()
+    const {user} = useContext(AuthContext)
+    const {data : isAdmin = [] , isLoading , refetch} = useQuery({
+        queryKey:['isAdmin'],
+        queryFn: async () => {
+           const res = await axiosSecure.get(`/admin/${user?.email}`)
+            return res.data.isAdmin
+        }
+    })
+    return [isAdmin,refetch,isLoading]
+}
+export default useAdmin;
