9988ffaa681a16e06b473ffb15eac7c6116e72e0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-03 09:58:16 +0600

    useSeller custom hook added

---
+++ src/CustomHook/useSeller/useSeller.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import useAxiosSecure from "../useAxiosSecure/useAxiosSecure";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
+
+
+function useSeller(){
+    const [axiosSecure] = useAxiosSecure()
+    const {user} = useContext(AuthContext)
+    const {data : isSeller = [] , isLoading , refetch} = useQuery({
+        queryKey:['isAdmin'],
+        queryFn: async () => {
+           const res = await axiosSecure.get(`/admin/${user?.email}`)
+            return res.data.isSeller
+        }
+    })
+    return [isSeller,refetch,isLoading]
+}
+
+export default useSeller;
