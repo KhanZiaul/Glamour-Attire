16162ca991b714a56be7cdddf25bd412edfb9f26
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-04 16:06:17 +0600

    MyNewProducts updated

--- src/Pages/DashBoard/SellerPanel/MyNewProducts/MyNewProducts.jsx
+++ src/Pages/DashBoard/SellerPanel/MyNewProducts/MyNewProducts.jsx
@@ -1,38 +1,52 @@
-import useAxiosSecure from "../../../../Hoooks/useAxiosSecure/useAxiosSecure";
+import useNewProducts from "../../../../CustomHook/useNewProducts/useNewProducts";
+import { BsFillTrashFill } from 'react-icons/bs'
 
 const MyNewProducts = () => {
+    const [newProducts, refetch] = useNewProducts()
+    console.log(newProducts)
 
-    
-  
     return (
         <div>
             <div className="overflow-x-auto">
                 <table className="table bg-white">
                     {/* head */}
                     <thead>
-                        <tr className="uppercase">
+                        <tr className="uppercase bg-[#1B6B93] text-white">
                             <th>#</th>
-                            <th>language</th>
-                            <th>Available Seats</th>
-                            <th>Enrolled Students</th>
-                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Brand</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>is Approved ?</th>
                             <th>Feedback</th>
                             <th>Update</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            myClasses?.map((myClass, index) => {
+                            newProducts?.map((myProduct, index) => {
                                 return (
-                                    <tr key={myClass._id}>
+                                    <tr key={myProduct._id}>
                                         <th>{index + 1}</th>
-                                        <td>{myClass.name}</td>
-                                        <td>{myClass.availableSeats}</td>
-                                        <td>{myClass.students}</td>
-                                        <td className="font-bold">{myClass.status}</td>
-                                        <td >{myClass.feedback ? myClass.feedback : ''}</td>
-                                        <td><button className="btn-sm">Update</button></td>
-
+                                        <td>
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={myProduct.img} alt="product image" />
+                                                </div>
+                                            </div></td>
+                                        <td>{myProduct.brand}</td>
+                                        <td>{myProduct.productName}</td>
+                                        <td>${myProduct.price}</td>
+                                        <td className="font-bold">{myProduct.isApproved}</td>
+                                        <td className="font-semibold">{myProduct.feedback ? myProduct.feedback : 'No Feedback'}
+                                        </td>
+                                        <td>
+                                            <button className="btn btn-outline btn-success btn-xs" >Update</button>
+                                        </td>
+                                        <td>
+                                            <BsFillTrashFill className="w-6 h-6 cursor-pointer text-[#FF6464]"></BsFillTrashFill>
+                                        </td>
                                     </tr>
                                 )
                             })
