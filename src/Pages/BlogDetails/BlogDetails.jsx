cf49ab0e3fd1f5c37e58f8bbf8292537937270f5
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-09 02:26:17 +0600

    BlogDetails Component updated

---
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -0,0 +1,22 @@
+import blog from '../../assets/img/banner/b8.jpg'
+
+const BlogDetails = () => {
+    return (
+        <div className="pt-12 lg:pt-16 mb-16">
+            <div className="hero h-[300px] mb-10" style={{ backgroundImage: `url(${blog})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">#stayhome</h1>
+                        <p className="mb-5">Save more with coupons & up to 70% off!</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default BlogDetails;
