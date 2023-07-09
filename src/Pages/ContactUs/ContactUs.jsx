1d13d23f8654e6b4fb0ed9d9abd0eb73ad1830e2
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-10 04:35:08 +0600

    contat updated

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import contact from '../../assets/img/about/contact.jpg'
 import talk from '../../assets/img/about/talk.jpg'
+import email from '../../assets/img/about/email.jpg'
 
 const ContactUs = () => {
     return (
@@ -14,8 +15,8 @@ const ContactUs = () => {
             </div>
 
             <div className='flex flex-col lg:flex-row items-center mx-6 gap-8'>
-                <div className='lg:w-[80%]'>
-                    <img className='rounded-xl' src={talk} alt="" />
+                <div className='lg:w-[80%] w-full'>
+                    <img src={talk} alt="" />
                 </div>
 
                 <div className='space-y-3'>
@@ -38,8 +39,42 @@ const ContactUs = () => {
                 </div>
             </div>
 
+            <div className='flex flex-col lg:flex-row items-center mx-6 gap-8 mt-20'>
+                <div data-aos="fade-down"
+                    data-aos-easing="linear"
+                    data-aos-duration="1500" className='mt-28 flex flex-col-reverse lg:flex-row'>
+                    <div className='w-full md:w-2/4 mx-auto border-2 p-10 rounded-xl shadow-2xl border-[#3abc47]'>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div>
+                                <div className="flex items-center border-b border-slate-700 py-2 mb-4 pt-10">
+                                    <input className="shadow-2xl appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Enter your name" name="user_name" aria-label="Full name" required />
+                                </div>
+
+                                <div className='mb-8'>
+                                    <div className="flex items-center border-b  border-slate-700 py-2">
+                                        <input className="shadow-2xl appearance-none bg-transparent border-none w-full  mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" name="user_email" placeholder="Enter your email" aria-label="Full name" />
+                                    </div>
+                                </div>
+
+                                <textarea name="message" placeholder="Message" className=" textarea shadow-2xl textarea-bordered textarea-lg w-full appearance-none bg-transparent border-nonetext-gray-700 mr-3 py-3 px-5 leading-tight focus:outline-none" aria-label="Full name">
+                                </textarea>
+
+                                <div className="form-control mt-6">
+                                    <div className='inline-block shadow-2xl'>
+                                        <button className="bg-[#68d372] hover:bg-[#3abc47] duration-200 px-5 py-3 rounded-md flex items-center justify-center gap-2"><BsSend className='w-6 h-6'></BsSend> Send Message</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='lg:w-[80%] w-full'>
+                        <img src={email} alt="" />
+                    </div>
+                </div>
+
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default ContactUs;
