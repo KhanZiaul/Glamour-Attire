4133d909d1c66ec38da7f2b916673d5d7769c122
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-19 17:51:32 +0600

    error page done

--- src/Pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/Pages/Shared/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import error from '../../../assets/Images/error.jpg'
+import error from '../../../assets/img/error/error.jpg'
 
 const ErrorPage = () => {
     return (
         <div>
-            <img className='lg:w-[80vw] lg:h-[80vh] mx-auto' src={error} alt="error-image" />
-            <div className='flex justify-center h-[20vh]'>
+            <img className='lg:w-[80vw] lg:h-[100vh] mx-auto' src={error} alt="error-image" />
+            <div className='flex justify-center h-[30vh]'>
                 <Link to='/'>
                     <button className='btn btn-primary'>BACK TO HOME</button>
                 </Link>
